dc1019adcc00841ada3b78aa33703da39336a3c0
Scott Erickson <sderickson@gmail.com>
2026-02-19 15:57:56 -0800

    step 3.2

--- recipes/service/identity/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "vitest";
-import * as exports from "@sderickson/recipes-identity";
-
-describe("@sderickson/recipes-identity", () => {
-  it("should be defined", () => {
-    expect(exports).toBeDefined();
-  });
-});
