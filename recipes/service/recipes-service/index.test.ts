64ac445b3dd0e06ed4154228adaba8895184748f
Scott Erickson <sderickson@gmail.com>
2026-02-19 09:50:09 -0800

    Revert "Create a new product"
    This reverts commit 58ce8652d1d1d117d072d1a4ba5f697e7fde68e4.

---
+++ recipes/service/recipes-service/index.test.ts
@@ -0,0 +1,8 @@
+import { expect, describe, it } from "vitest";
+import * as mainExports from "@sderickson/recipes-service";
+
+describe("@sderickson/recipes-service", () => {
+  it("should be defined", () => {
+    expect(mainExports).toBeDefined();
+  });
+});
