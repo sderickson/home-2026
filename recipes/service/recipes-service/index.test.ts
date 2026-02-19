58ce8652d1d1d117d072d1a4ba5f697e7fde68e4
Scott Erickson <sderickson@gmail.com>
2026-02-18 19:22:44 -0800

    Create a new product

--- recipes/service/recipes-service/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, describe, it } from "vitest";
-import * as mainExports from "@sderickson/recipes-service";
-
-describe("@sderickson/recipes-service", () => {
-  it("should be defined", () => {
-    expect(mainExports).toBeDefined();
-  });
-});
