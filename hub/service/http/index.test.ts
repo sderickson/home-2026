fde5d0ce165cd743774cc529f61fececb55dc0d3
Scott Erickson <sderickson@gmail.com>
2026-02-19 09:33:29 -0800

    Init @sderickson/hub-http.
    * Upsert 6 templates.
    * Change working directory to hub/service/http
    * Run `npm install`
    * Run `npm test`

---
+++ hub/service/http/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import * as exports from "@sderickson/hub-http";
+
+describe("@sderickson/hub-http", () => {
+  it("should be defined", () => {
+    expect(exports).toBeDefined();
+  });
+});
