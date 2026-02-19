b82989dde4aa3b7497f1311dd6e342f38cb2a19e
Scott Erickson <sderickson@gmail.com>
2026-02-19 09:33:47 -0800

    Init @sderickson/hub-admin-spa.
    * Upsert 54 templates.
    * Change working directory to hub/clients/admin
    * Run `npm install`
    * Change working directory to hub/clients/build
    * Run `npm install @sderickson/hub-admin-spa`

---
+++ hub/clients/admin/router.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { adminLinks } from "@sderickson/hub-links";
+import { PageNotFound } from "@saflib/vue/components";
+
+// TODO: remove this log once adminLinks is being used by the routes
+console.log("adminLinks:", adminLinks);
+
+// BEGIN SORTED WORKFLOW AREA page-imports FOR vue/add-view
+
+// END WORKFLOW AREA
+
+export const createAdminRouter = () => {
+  const routes = [
+    // BEGIN WORKFLOW AREA page-routes FOR vue/add-view
+
+
+
+
+    // END WORKFLOW AREA
+    { path: "/:pathMatch(.*)*", component: PageNotFound },
+  ];
+  return createRouter({
+    history: createWebHistory("/"),
+    routes,
+  });
+};
--- hub/clients/links/index.ts
+++ hub/clients/links/index.ts
@@ -1,3 +1,4 @@
 // BEGIN SORTED WORKFLOW AREA subdomain-links FOR vue/add-spa
+export { adminLinks } from "./admin-links.ts";
 export { rootLinks } from "./root-links.ts";
 // END WORKFLOW AREA
