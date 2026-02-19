9aa569c00f4d8419f991a060268fd6852729bb28
Scott Erickson <sderickson@gmail.com>
2026-02-18 19:44:54 -0800

    Init @sderickson/notebook-admin-spa.
    * Upsert 54 templates.
    * Change working directory to notebook/clients/admin
    * Run `npm install`
    * Change working directory to notebook/clients/build
    * Run `npm install @sderickson/notebook-admin-spa`

---
+++ notebook/clients/admin/strings.ts
@@ -0,0 +1,12 @@
+import { notebook_common_strings } from "../common/strings.ts";
+
+// BEGIN SORTED WORKFLOW AREA page-string-imports FOR vue/add-view
+
+// END WORKFLOW AREA
+
+export const admin_strings = {
+  ...notebook_common_strings,
+  // BEGIN SORTED WORKFLOW AREA page-string-object FOR vue/add-view
+
+  // END WORKFLOW AREA
+};
