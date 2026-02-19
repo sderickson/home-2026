932921d7455f215920bb9586df881963a46b87ff
Scott Erickson <sderickson@gmail.com>
2026-02-18 19:44:38 -0800

    Init @sderickson/notebook-http.
    * Upsert 6 templates.
    * Change working directory to notebook/service/http
    * Run `npm install`
    * Run `npm test`

---
+++ notebook/service/http/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import * as exports from "@sderickson/notebook-http";
+
+describe("@sderickson/notebook-http", () => {
+  it("should be defined", () => {
+    expect(exports).toBeDefined();
+  });
+});
