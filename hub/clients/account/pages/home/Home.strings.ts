925663b382d1a0818033cb70bb07cf34ba0de5b2
Scott Erickson <sderickson@gmail.com>
2026-02-19 09:44:00 -0800

    Create a new page, dialog, or other view in a SAF-powered Vue SPA, using a template and renaming placeholders.
    * Upsert 54 templates.
    * Update **Home.vue** to render the page:
    * Now that the view is implemented, extract testable logic and write tests.
    * Run `npm run test`
    * Run `npm run typecheck`

---
+++ hub/clients/account/pages/home/Home.strings.ts
@@ -0,0 +1,6 @@
+export const home_page = {
+  title: "Account",
+  subtitle: "Manage your profile and password.",
+  link_profile: "Profile",
+  link_password: "Password",
+};
---
+++ hub/clients/account/pages/home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <h1>{{ t(strings.title) }}</h1>
+    <p>{{ t(strings.subtitle) }}</p>
+    <v-btn v-bind="profileLinkProps" variant="text" class="mr-2">
+      {{ t(strings.link_profile) }}
+    </v-btn>
+    <v-btn v-bind="passwordLinkProps" variant="text">
+      {{ t(strings.link_password) }}
+    </v-btn>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { home_page as strings } from "./Home.strings.ts";
+import { useHomeLoader } from "./Home.loader.ts";
+import { useReverseT } from "@sderickson/hub-account-spa/i18n";
+import {
+  assertProfileLoaded,
+  getProfileLinkProps,
+  getPasswordLinkProps,
+} from "./Home.logic.ts";
+
+const { t } = useReverseT();
+const { profileQuery } = useHomeLoader();
+
+assertProfileLoaded(profileQuery.data.value);
+
+const profileLinkProps = getProfileLinkProps();
+const passwordLinkProps = getPasswordLinkProps();
+</script>
