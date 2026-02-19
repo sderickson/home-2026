cf1c758fad6b2d618a9690f8117bb2565032f126
Scott Erickson <sderickson@gmail.com>
2026-02-18 19:28:19 -0800

    Create a new product

--- recipes/clients/auth/AuthSpa.vue
+++ recipes/clients/auth/AuthSpa.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { RecipesLayout } from "@sderickson/recipes-clients-common";
+import { useProfile } from "@saflib/auth";
+
+const profileQuery = useProfile();
+const loggedIn = computed(() => !!profileQuery.data?.value);
 </script>
 
 <template>
-  <RecipesLayout>
+  <RecipesLayout :logged-in="loggedIn">
     <router-view />
   </RecipesLayout>
 </template>
