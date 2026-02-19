48524494bf492aaa4f87f43d64659497191b2422
Scott Erickson <sderickson@gmail.com>
2026-02-19 09:34:09 -0800

    Init @sderickson/hub-identity.
    * Upsert 12 templates.
    * Change working directory to hub/service/identity
    * Run `npm exec saf-env generate`
    * Run `npm install`

---
+++ hub/service/identity/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import * as exports from "@sderickson/hub-identity";
+
+describe("@sderickson/hub-identity", () => {
+  it("should be defined", () => {
+    expect(exports).toBeDefined();
+  });
+});
