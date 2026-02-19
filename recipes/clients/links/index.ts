716c7ac84eca0a08a28868ab11419282c5ec616e
Scott Erickson <sderickson@gmail.com>
2026-02-18 19:22:27 -0800

    Init @sderickson/recipes-app-spa.
    * Upsert 54 templates.
    * Change working directory to recipes/clients/app
    * Run `npm install`
    * Change working directory to recipes/clients/build
    * Run `npm install @sderickson/recipes-app-spa`

--- recipes/clients/links/index.ts
+++ recipes/clients/links/index.ts
@@ -1,4 +1,5 @@
 // BEGIN SORTED WORKFLOW AREA subdomain-links FOR vue/add-spa
 export { adminLinks } from "./admin-links.ts";
+export { appLinks } from "./app-links.ts";
 export { rootLinks } from "./root-links.ts";
 // END WORKFLOW AREA
