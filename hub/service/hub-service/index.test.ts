5a84b8f6a690c63d36a46905f3d7df3e98dc7f9b
Scott Erickson <sderickson@gmail.com>
2026-02-19 09:34:10 -0800

    Run `rm -rf ./hub/service/hub-service`

--- hub/service/hub-service/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, describe, it } from "vitest";
-import * as mainExports from "@sderickson/hub-service";
-
-describe("@sderickson/hub-service", () => {
-  it("should be defined", () => {
-    expect(mainExports).toBeDefined();
-  });
-});
