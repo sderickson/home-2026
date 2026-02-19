f059fe9c20568e5ec54279d251f933a38db45ded
Scott Erickson <sderickson@gmail.com>
2026-02-18 19:45:00 -0800

    Init @sderickson/notebook-app-spa.
    * Upsert 54 templates.
    * Change working directory to notebook/clients/app
    * Run `npm install`
    * Change working directory to notebook/clients/build
    * Run `npm install @sderickson/notebook-app-spa`

---
+++ notebook/clients/app/router.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { appLinks } from "@sderickson/notebook-links";
+import { PageNotFound } from "@saflib/vue/components";
+
+// TODO: remove this log once appLinks is being used by the routes
+console.log("appLinks:", appLinks);
+
+// BEGIN SORTED WORKFLOW AREA page-imports FOR vue/add-view
+
+// END WORKFLOW AREA
+
+export const createAppRouter = () => {
+  const routes = [
+    // BEGIN WORKFLOW AREA page-routes FOR vue/add-view
+
+
+
+
+    // END WORKFLOW AREA
+    { path: "/:pathMatch(.*)*", component: PageNotFound },
+  ];
+  return createRouter({
+    history: createWebHistory("/"),
+    routes,
+  });
+};
