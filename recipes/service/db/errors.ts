55dba610e49cd0e563dc3182bc762ff26d3a2a01
Scott Erickson <sderickson@gmail.com>
2026-02-18 19:21:51 -0800

    Init @sderickson/recipes-db.
    * Upsert 11 templates.
    * Change working directory to recipes/service/db
    * Run `npm install`
    * Run `npm run generate`
    * Run `mkdir -p data`
    * Run `touch data/.gitkeep`
    * Run `npm test`

---
+++ recipes/service/db/errors.ts
@@ -0,0 +1,9 @@
+import { HandledDatabaseError } from "@saflib/drizzle";
+
+/**
+ * Superclass for all handled recipes db errors
+ */
+export class RecipesDatabaseError extends HandledDatabaseError {}
+
+// TODO: Add specific error classes for your database
+export class StubError extends RecipesDatabaseError {}
