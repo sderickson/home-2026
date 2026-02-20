e01129da006e8f47ba968cad98c7b7f2626b7fc2
Scott Erickson <sderickson@gmail.com>
2026-02-20 14:09:11 -0800

    Add new query recipe/list to the database.
    * Upsert 6 templates.
    * Implement the new query following the documentation guidelines.
    * Run `npm run typecheck`
    * Implement the generated test file.
    * Run `npm run typecheck`
    * Run `npm run test`

--- recipes/service/db/index.ts
+++ recipes/service/db/index.ts
@@ -5,5 +5,5 @@ import { recipesDbManager } from "./instances.ts";
 export const recipesDb = recipesDbManager.publicInterface();
 
 // BEGIN SORTED WORKFLOW AREA query-exports FOR drizzle/add-query
-
+export * from "./queries/recipe/index.ts";
 // END WORKFLOW AREA
---
+++ recipes/service/db/queries/recipe/index.ts
@@ -0,0 +1,9 @@
+// BEGIN SORTED WORKFLOW AREA query-exports FOR drizzle/add-query
+import { listRecipes } from "./list.ts";
+// END WORKFLOW AREA
+
+export const recipeQueries = {
+  // BEGIN SORTED WORKFLOW AREA query-object FOR drizzle/add-query
+  listRecipes,
+  // END WORKFLOW AREA
+};
