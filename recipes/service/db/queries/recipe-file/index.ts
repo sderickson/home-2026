d89f629c10cb8ade20fd4d3e8748f9abd3b70453
Scott Erickson <sderickson@gmail.com>
2026-02-23 13:32:47 -0800

    Add new query recipe-file/insert to the database.
    * Upsert 6 templates.
    * Implement the new query following the documentation guidelines.
    * Run `npm run typecheck`
    * Implement the generated test file.
    * Run `npm run typecheck`
    * Run `npm run test`

--- recipes/service/db/queries/recipe-file/index.ts
+++ recipes/service/db/queries/recipe-file/index.ts
@@ -1,9 +1,11 @@
 // BEGIN SORTED WORKFLOW AREA query-exports FOR drizzle/add-query
+import { insertRecipeFile } from "./insert.ts";
 import { listRecipeFile } from "./list.ts";
 // END WORKFLOW AREA
 
 export const recipeFileQueries = {
   // BEGIN SORTED WORKFLOW AREA query-object FOR drizzle/add-query
+  insertRecipeFile,
   listRecipeFile,
   // END WORKFLOW AREA
 };
