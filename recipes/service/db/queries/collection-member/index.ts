77a501d66701049b3eb4625e47d816586b207e43
Scott Erickson <sderickson@gmail.com>
2026-03-12 10:51:20 -0700

    Add new query collection-member/add to the database.
    * Upsert 6 templates.
    * Implement the new query following the documentation guidelines.
    * Run `npm run typecheck`
    * Implement the generated test file.
    * Run `npm run typecheck`
    * Run `npm run test`

--- recipes/service/db/queries/collection-member/index.ts
+++ recipes/service/db/queries/collection-member/index.ts
@@ -1,10 +1,12 @@
 // BEGIN SORTED WORKFLOW AREA query-exports FOR drizzle/add-query
+import { addCollectionMember } from "./add.ts";
 import { getByCollectionAndEmailCollectionMember } from "./get-by-collection-and-email.ts";
 import { listCollectionMember } from "./list.ts";
 // END WORKFLOW AREA
 
 export const collectionMemberQueries = {
   // BEGIN SORTED WORKFLOW AREA query-object FOR drizzle/add-query
+  addCollectionMember,
   getByCollectionAndEmailCollectionMember,
   listCollectionMember,
   // END WORKFLOW AREA
