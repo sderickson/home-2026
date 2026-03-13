4c601464a543b9c90128ffb0799a354d170a6925
Scott Erickson <sderickson@gmail.com>
2026-03-13 14:48:00 -0700

    Add new query menu/list-public to the database.
    * Upsert 6 templates.
    * Implement the new query following the documentation guidelines.
    * Run `npm run typecheck`
    * Implement the generated test file.
    * Run `npm run typecheck`
    * Run `npm run test`

--- recipes/service/db/queries/menu/index.ts
+++ recipes/service/db/queries/menu/index.ts
@@ -1,9 +1,11 @@
 // BEGIN SORTED WORKFLOW AREA query-exports FOR drizzle/add-query
 import { listByCollectionIdMenu } from "./list-by-collection-id.ts";
+import { listPublicMenu } from "./list-public.ts";
 // END WORKFLOW AREA
 
 export const menuQueries = {
   // BEGIN SORTED WORKFLOW AREA query-object FOR drizzle/add-query
   listByCollectionIdMenu,
+  listPublicMenu,
   // END WORKFLOW AREA
 };
