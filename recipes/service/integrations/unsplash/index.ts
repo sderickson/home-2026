22da0fe2e4e44bc8eb7e70ba779df7fa737393fa
Scott Erickson <sderickson@gmail.com>
2026-03-14 09:12:30 -0700

    add workflow area

--- recipes/service/integrations/unsplash/index.ts
+++ recipes/service/integrations/unsplash/index.ts
@@ -1,3 +1,6 @@
 export { unsplash, isMocked } from "./client.ts";
 export type { ScopedUnsplashClient } from "./client.ts";
 export { ping } from "./calls/ping.ts";
+
+// BEGIN SORTED WORKFLOW AREA call-exports FOR integrations/add-call
+// END WORKFLOW AREA
