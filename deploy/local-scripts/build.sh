c8d787241690460b61796a52b74545324f4f1c84
Scott Erickson <sderickson@gmail.com>
2026-03-19 11:20:04 -0700

    bring back tags

--- deploy/local-scripts/build.sh
+++ deploy/local-scripts/build.sh
@@ -10,14 +10,24 @@ npx saf-git-hashes
 # Build dependent images
 
 # BEGIN WORKFLOW AREA build-product-dependencies FOR product/init
-docker build -t sderickson-blog-client:latest -f ./blog/Dockerfile . --platform linux/amd64
-docker build -t sderickson-recipes-monolith:latest -f ./recipes/service/monolith/Dockerfile . --platform linux/amd64
-docker build -t sderickson-notebook-monolith:latest -f ./notebook/service/monolith/Dockerfile . --platform linux/amd64
-docker build -t sderickson-hub-monolith:latest -f ./hub/service/monolith/Dockerfile . --platform linux/amd64
+docker build -f ./blog/Dockerfile . --platform linux/amd64 \
+	-t sderickson-blog-client:latest \
+	-t "$CONTAINER_REGISTRY/sderickson-blog-client:latest"
+docker build -f ./recipes/service/monolith/Dockerfile . --platform linux/amd64 \
+	-t sderickson-recipes-monolith:latest \
+	-t "$CONTAINER_REGISTRY/sderickson-recipes-monolith:latest"
+docker build -f ./notebook/service/monolith/Dockerfile . --platform linux/amd64 \
+	-t sderickson-notebook-monolith:latest \
+	-t "$CONTAINER_REGISTRY/sderickson-notebook-monolith:latest"
+docker build -f ./hub/service/monolith/Dockerfile . --platform linux/amd64 \
+	-t sderickson-hub-monolith:latest \
+	-t "$CONTAINER_REGISTRY/sderickson-hub-monolith:latest"
 
 # END WORKFLOW AREA
 
 # Build reverse proxy image
-docker build -t sderickson-caddy:latest -f ./deploy/Dockerfile.prod . --platform linux/amd64
+docker build -f ./deploy/Dockerfile.prod . --platform linux/amd64 \
+	-t sderickson-caddy:latest \
+	-t "$CONTAINER_REGISTRY/sderickson-caddy:latest"
 
 # Note: sometimes need to run with --no-cache if cache got into a weird state from cancelling mid-build
