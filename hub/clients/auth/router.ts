581adfb1a4c7a9cb294ac708359e5fa6b7b076af
Scott Erickson <sderickson@gmail.com>
2026-03-23 08:27:17 -0700

    tweak

--- hub/clients/auth/router.ts
+++ hub/clients/auth/router.ts
@@ -5,15 +5,16 @@ import { appLinks, rootLinks } from "@sderickson/hub-links";
 import KratosTest from "./pages/KratosTest.vue";
 
 export const createAuthRouter = (options?: { history?: RouterHistory }) => {
+  const routes = [
+    // BEGIN WORKFLOW AREA page-routes FOR vue/add-view
+    { path: "/kratos-test", component: KratosTest },
+    // END WORKFLOW AREA
+  ];
   return createSaflibAuthRouter({
     loginRedirect: linkToHrefWithHost(appLinks.home),
     registerRedirect: linkToHrefWithHost(appLinks.home),
     logoutRedirect: linkToHrefWithHost(rootLinks.home),
     history: options?.history,
-    additionalRoutes: [
-      // BEGIN WORKFLOW AREA page-routes FOR vue/add-view
-      { path: "/kratos-test", component: KratosTest },
-      // END WORKFLOW AREA
-    ],
+    additionalRoutes: routes,
   });
 };
