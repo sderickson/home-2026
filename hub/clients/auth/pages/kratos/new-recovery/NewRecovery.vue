2a2a7aad00392f5a74697e19f1d3404d782ef598
Scott Erickson <sderickson@gmail.com>
2026-03-27 17:13:39 -0700

    add error

--- hub/clients/auth/pages/kratos/new-recovery/NewRecovery.vue
+++ hub/clients/auth/pages/kratos/new-recovery/NewRecovery.vue
@@ -4,15 +4,22 @@
       v-if="queryData instanceof RecoveryFlowCreatedResult"
       :result="queryData"
     />
+    <SessionAlreadyAvailableComponent
+      v-else-if="queryData instanceof SessionAlreadyAvailable"
+    />
     <UnhandledResponsePanel v-else :result="queryData" />
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { RecoveryFlowCreated as RecoveryFlowCreatedResult } from "@saflib/ory-kratos-sdk";
+import {
+  RecoveryFlowCreated as RecoveryFlowCreatedResult,
+  SessionAlreadyAvailable,
+} from "@saflib/ory-kratos-sdk";
 import { useNewRecoveryLoader } from "./NewRecovery.loader.ts";
 import UnhandledResponsePanel from "../common/UnhandledResponsePanel.vue";
 import RecoveryFlowCreatedView from "./RecoveryFlowCreated.vue";
+import SessionAlreadyAvailableComponent from "../common/SessionAlreadyAvailable.vue";
 import { computed, toValue } from "vue";
 
 const { createRecoveryFlowQuery } = useNewRecoveryLoader();
