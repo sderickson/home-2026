60220412c4c11522310e4e487099cbe20d1b5b79
Scott Erickson <sderickson@gmail.com>
2026-04-01 13:08:27 -0700

    Move Kratos auth SPA to SAF (#51)
    Transfer to SAF. Paired with
    https://github.com/sderickson/saflib/pull/262
    
    Having built the spa to work with my personal home page, move to SAF for
    use in other projects.

--- hub/clients/auth/pages/kratos/verification/VerificationIntro.strings.ts
+++
@@ -1,9 +0,0 @@
-export const verification_intro = {
-  title: "Verify your email",
-  link_login: "Sign in instead",
-};
-
-export const verification_verified = {
-  title: "You are already verified",
-  cta_continue: "Continue",
-};
--- hub/clients/auth/pages/kratos/verify-wall/VerifyWallBlockedBody.strings.ts
+++
@@ -1,3 +0,0 @@
-export const verify_wall_blocked_body = {
-  body: "You are signed in, but your email is not verified yet. Check your inbox for a verification email. Some features may be limited until you verify.",
-};
