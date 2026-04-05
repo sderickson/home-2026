f78bf2d3ef3263986ebf0ef5e3e0f96cc04e8d1d
Scott Erickson <sderickson@gmail.com>
2026-04-05 11:06:09 -0700

    extend test

--- hub/clients/auth/e2e/register.spec.ts/register-logout-login.spec.ts
+++ hub/clients/auth/e2e/register.spec.ts/register-logout-login.spec.ts
@@ -1,23 +1,32 @@
 import { test as base, expect } from "@playwright/test";
 import {
+  loginPageFixture,
   registrationPageFixture,
   verifyWallPageFixture,
+  type LoginPageFixture,
   type RegistrationPageFixture,
   type VerifyWallPageFixture,
 } from "@saflib/ory-kratos-spa/fixtures";
 import { getUniqueEmail } from "@saflib/playwright";
 
 type RegisterFixtures = {
+  loginPage: LoginPageFixture;
   registrationPage: RegistrationPageFixture;
   verifyWallPage: VerifyWallPageFixture;
 };
 
 const test = base.extend<RegisterFixtures>({
+  loginPage: loginPageFixture,
   registrationPage: registrationPageFixture,
   verifyWallPage: verifyWallPageFixture,
 });
 
-test("register", async ({ page, registrationPage, verifyWallPage }) => {
+test("register, logout, and login", async ({
+  page,
+  loginPage,
+  registrationPage,
+  verifyWallPage,
+}) => {
   const uniqueEmail = getUniqueEmail();
 
   await page.goto("http://docker.localhost/");
@@ -26,5 +35,10 @@ test("register", async ({ page, registrationPage, verifyWallPage }) => {
   await registrationPage.completeRegistration(uniqueEmail, "packtofu");
   await verifyWallPage.toBeVisible();
   await verifyWallPage.clickContinueToApp();
+  await expect(page.getByRole("heading", { name: "App Home" })).toBeVisible();
+  await page.getByRole("banner").getByRole("link", { name: "Logout" }).click();
+  await page.getByRole("link", { name: "Log in" }).click();
+  await loginPage.toBeVisible();
+  await loginPage.signInWithPassword(uniqueEmail, "packtofu");
   await expect(page.getByRole("heading", { name: "App Home" })).toBeVisible();
 });
