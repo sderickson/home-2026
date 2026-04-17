2da51e0962fb780d305b9d07ce5d1c7981b09f04
Scott Erickson <sderickson@gmail.com>
2026-04-17 07:24:40 -0700

    undo changes

--- recipes/clients/app/pages/home/HomeAsync.vue
+++ recipes/clients/app/pages/home/HomeAsync.vue
@@ -12,10 +12,5 @@ import { useHomeLoader } from "./Home.loader.ts";
 import { AsyncPage } from "@saflib/vue/components";
 import AppHomeError from "./AppHomeError.vue";
 
-const Home = defineAsyncComponent(async () => {
-  console.log("starting code sleep");
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log("ending code sleep");
-  return import("./Home.vue");
-});
+const Home = defineAsyncComponent(() => import("./Home.vue"));
 </script>
--- recipes/service/sdk/requests/collections/list.ts
+++ recipes/service/sdk/requests/collections/list.ts
@@ -5,11 +5,6 @@ import { getClient } from "../../client.ts";
 export const listCollectionsQuery = () => {
   return queryOptions({
     queryKey: ["collections", "list"],
-    queryFn: async () => {
-      console.log("starting data sleep");
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      console.log("ending data sleep");
-      return handleClientMethod(getClient().GET("/collections"));
-    },
+    queryFn: async () => handleClientMethod(getClient().GET("/collections")),
   });
 };
